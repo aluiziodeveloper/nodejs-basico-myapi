09a5dfeb9080ae8167ed7aa97cfd3bcf6bf82389
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-06-23 18:40:38 -0300

    Add lib dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
@@ -10,6 +11,6 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Olá Dev!' })
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000! 🏆')
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}! 🏆`)
 })
