3ec8247e32d7374088eee198399ed3538d55feff
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-06-24 00:58:55 -0300

    Add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new Error('Acesso negado')
   return response.json({ message: 'Olá Dev!' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,31 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      })
+    }
+    console.log(error)
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    })
+  },
+)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}! 🏆`)
