7fcfa251612c2966adf582a047a1f8aec5713942
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-06-24 00:26:38 -0300

    Config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,14 +2,13 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá Dev!' })
-})
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}! 🏆`)
