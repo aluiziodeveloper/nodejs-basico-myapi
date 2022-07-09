6cac02d46a4827a7969447af72df13c0fcb81d5f
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-09 16:17:19 -0300

    Install and config swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,16 @@
 import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
+import swaggerFile from '../../swagger.json'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 
 app.use(
