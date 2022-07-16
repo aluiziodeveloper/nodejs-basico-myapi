5565d0b26403c1e99e8f7e1cdfa3b2039bed25cb
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-16 09:50:43 -0300

    Create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1657974893356 } from './migrations/1657974893356-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1657974893356],
 })
---
+++ src/shared/typeorm/migrations/1657974893356-CreateRolesTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateRolesTable1657974893356 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roles')
+  }
+}
