697ce3e6e7822df394abedac3a9e203245d5297d
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-16 16:32:26 -0300

    Create user repository interface

---
+++ src/users/repositories/IUsersRepository.ts
@@ -0,0 +1,36 @@
+import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/User'
+
+export type CreateUserDTO = {
+  name: string
+  email: string
+  password: string
+  isAdmin: boolean
+  role: Role
+}
+
+export type PaginateParams = {
+  page: number
+  skip: number
+  take: number
+}
+
+export type UsersPaginateProperties = {
+  per_page: number
+  total: number
+  current_page: number
+  data: User[]
+}
+
+export interface IUsersRepository {
+  create({ name, email, password, isAdmin, role }: CreateUserDTO): Promise<User>
+  save(user: User): Promise<User>
+  findAll({
+    page,
+    skip,
+    take,
+  }: PaginateParams): Promise<UsersPaginateProperties>
+  findById(id: string): Promise<User | null>
+  findByName(name: string): Promise<User | null>
+  delete(user: User): Promise<void>
+}
