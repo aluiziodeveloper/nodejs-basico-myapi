694e752e19ec3e70104b623837ccf5be397d6176
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-16 15:46:00 -0300

    Create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1657974893356 } from './migrations/1657974893356-CreateRolesTable'
+import { CreateUsersTable1657996796706 } from './migrations/1657996796706-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1657974893356],
+  migrations: [CreateRolesTable1657974893356, CreateUsersTable1657996796706],
 })
---
+++ src/shared/typeorm/migrations/1657996796706-CreateUsersTable.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUsersTable1657996796706 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+          },
+          {
+            name: 'email',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'string',
+          },
+          {
+            name: 'avatar',
+            type: 'string',
+            isNullable: true,
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
