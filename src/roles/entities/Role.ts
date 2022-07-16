da725535722ed20f345098ee17cff1b8bf4eb660
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-16 10:04:39 -0300

    Refactor role entity

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,8 +1,15 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+@Entity('roles')
 export class Role {
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   name: string
+
+  @CreateDateColumn()
   created_at: Date
 
   constructor() {
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
+import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1657974893356 } from './migrations/1657974893356-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1657974893356],
 })
