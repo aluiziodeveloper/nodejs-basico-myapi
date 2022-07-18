792385458e6f934ccfcfdd924f472d4735b9322e
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-18 10:59:39 -0300

    Add ListUsers usecase - 2

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { UsersRepository } from '@users/repositories/UsersRepository'
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
+import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 import { container } from 'tsyringe'
 
 container.registerSingleton<IUsersRepository>(
@@ -8,3 +9,4 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository,
 )
 container.registerSingleton('CreateUserController', CreateUserController)
+container.registerSingleton('ListUsersControllers', ListUsersController)
--- src/users/http/users.routes.ts
+++ src/users/http/users.routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
 import { container } from 'tsyringe'
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
+import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 
 const usersRouter = Router()
 const createUserController = container.resolve(CreateUserController)
+const listUsersController = container.resolve(ListUsersController)
 
 usersRouter.post(
   '/',
@@ -19,6 +21,19 @@ usersRouter.post(
   }),
   (request, response) => {
     return createUserController.handle(request, response)
+  },
+)
+
+usersRouter.get(
+  '/',
+  celebrate({
+    [Segments.QUERY]: {
+      page: Joi.number(),
+      limit: Joi.number(),
+    },
+  }),
+  (request, response) => {
+    return listUsersController.handle(request, response)
   },
 )
 
